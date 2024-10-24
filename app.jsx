3e78df04421a87bfc36c356f8bc7ca94fc659679
Ashish Dixit <163759660+Ashish123897@users.noreply.github.com>
2024-10-24 12:59:10 +0530

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,29 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Jack Bauer",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    phone: "Jack Bauer, portrayed by Kiefer Sutherland ",
+    email: "jack@nowhere.com",
+  },
+  {
+    id: 2,
+    name: "Beyonce",
+    imgURL:
+      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+    phone:
+      "Beyoncé, a multifaceted artist and cultural icon, is celebrated for her powerful voice, dynamic performances, and significant impact on music and fashion. Rising to fame as the lead singer of Destiny's Child ",
+    email: "b@beyonce.com",
+  },
+  {
+    id: 3,
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com",
+  },
+];
+
+export default contacts;
